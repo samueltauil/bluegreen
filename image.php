113c9b5030726a57787d334e345108bc7b2e5e11
Samuel Tauil <samueltauil@gmail.com>
2018-04-25 15:36:02 -0400

    Update image.php

--- image.php
+++ image.php
@@ -4,10 +4,12 @@ $im = imagecreatetruecolor(200, 200);
 $white = imagecolorallocate($im, 255, 255, 255);
 $blue = imagecolorallocate($im, 0, 0, 255);
 $green = imagecolorallocate($im, 0, 255, 0);
+$black = imagecolorallocate($im, 0, 0, 0);
 
 // Draw a filled rectangle
 //imagefilledrectangle($im, 0, 0, 199, 199, $blue);
-imagefilledrectangle($im, 0, 0, 199, 199, $green);
+//imagefilledrectangle($im, 0, 0, 199, 199, $green);
+imagefilledrectangle($im, 0, 0, 199, 199, $black);
 
 // Save the image
 header('Content-Type: image/png');
